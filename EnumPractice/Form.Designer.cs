d7b91cf388a182741fad3c758282b6133d0283fe
Ayşegül YANIK <aysegul_demirtas@hotmail.com.tr>
2021-03-15 15:28:10 +0300

    Enum Translation
    Doctype and mimetype translation by enum type. Variables are in Turkish, you can modify as your need.

---
+++ EnumPractice/Form.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace EnumPractice
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dosyaTipi = new System.Windows.Forms.Label();
+            this.dosyaUzantisi = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(119, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(170, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dosya Tipi";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(170, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Dosya Uzantısı";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(253, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "DÖNÜŞTÜR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dosyaTipi
+            // 
+            this.dosyaTipi.AutoSize = true;
+            this.dosyaTipi.Location = new System.Drawing.Point(334, 33);
+            this.dosyaTipi.Name = "dosyaTipi";
+            this.dosyaTipi.Size = new System.Drawing.Size(57, 13);
+            this.dosyaTipi.TabIndex = 5;
+            this.dosyaTipi.Text = "Dosya Tipi";
+            this.dosyaTipi.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // dosyaUzantisi
+            // 
+            this.dosyaUzantisi.AutoSize = true;
+            this.dosyaUzantisi.Location = new System.Drawing.Point(334, 66);
+            this.dosyaUzantisi.Name = "dosyaUzantisi";
+            this.dosyaUzantisi.Size = new System.Drawing.Size(77, 13);
+            this.dosyaUzantisi.TabIndex = 6;
+            this.dosyaUzantisi.Text = "Dosya Uzantısı";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 162);
+            this.Controls.Add(this.dosyaUzantisi);
+            this.Controls.Add(this.dosyaTipi);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form";
+            this.Text = "ENUM TRANSLATION";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label dosyaTipi;
+        private System.Windows.Forms.Label dosyaUzantisi;
+    }
+}
+
---
+++ EnumPractice/Form.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnumPractice
+{
+    public enum DocumentType
+    {
+        [Description("Ameliyat Notu")]
+        AmeliyatNotu,
+
+        [Description("Anestezi Notu")]
+        AnesteziNotu,
+
+        [Description("Konsültasyon Raporu")]
+        KonsultasyonRp,
+
+        [Description("Epikriz Notu")]
+        EpikrizNotu,
+
+        [Description("Fatura")]
+        Fatura,
+
+        [Description("Görüşme Sonucu?")]
+        GorSonuc,
+
+        [Description("Hasta Bilgi Formu")]
+        HastaBilgiFormu,
+
+        [Description("Diğer Tazmin Evrak")]
+        DigerTazmEvr,
+
+        [Description("İbraname")]
+        ibraname,
+
+        [Description("Reçete")]
+        Recete,
+
+        [Description("Tetkik Sonucu")]
+        TetkikSonucu,
+
+        [Description("Patoloji Sonucu")]
+        PatolojiSonuc,
+
+        [Description("Poliklinik Kaydı")]
+        PolklinikKaydi,
+
+        [Description("Sigortalı Beyan")]
+        SigortaliBeyan,
+
+        [Description("Tedavi Planı")]
+        TedaviPlani,
+
+        [Description("Fatura Döküm")]
+        FaturaDokum,
+
+        [Description("Açık Rıza Beyanı")]
+        AcikRizaBeyani
+}
+
+    public enum MimeType
+    {
+        [Description("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")]
+        sheet,
+
+        [Description("application/vnd.ms-excel")]
+        excel,
+
+        [Description("application/pdf")]
+        pdf, 
+
+        [Description("image/png")]
+        png, 
+
+        [Description("image/jpeg")]
+        jpeg, 
+
+        [Description("application/vnd.openxmlformats-officedocument.wordprocessingml.document")]
+        doc, 
+
+        [Description("application/msword")]
+        msword 
+    }
+
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+        public class Document
+        {
+            string mimetype;
+            string name;
+            byte[] bytes;
+        }
+
+        public class DocumentCollection
+        {
+            public List<Document> Documents { get; set; }
+        }
+
+        public static string GetDocumentType(Enum en)
+        {
+            Type type = en.GetType();
+            MemberInfo[] memInfo = type.GetMember(en.ToString());
+            if (memInfo != null && memInfo.Length > 0)
+            {
+                object[] attrs = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+                if (attrs != null && attrs.Length > 0)
+                    return ((DescriptionAttribute)attrs[0]).Description;
+            }
+            return en.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Yüklenen resmin uzantısına göre ve belge tipine göre işlem farklılık göstersin istediğimiz durumlarda, Enum kullanmak mantıklı.
+            string doktip = textBox1.Text;
+            DocumentType d_type;
+            switch (doktip)
+            {
+                case "AmeliyatNotu":
+                    d_type = DocumentType.AmeliyatNotu;
+                    break;
+                case "AnesteziNotu":
+                    d_type = DocumentType.AnesteziNotu;
+                    break;
+                case "DigerTazmEvr":
+                    d_type = DocumentType.DigerTazmEvr;
+                    break;
+                case "EpikrizNot":
+                    d_type = DocumentType.EpikrizNotu;
+                    break;
+                case "Fatura":
+                    d_type = DocumentType.Fatura;
+                    break;
+                case "FaturaDokum":
+                    d_type = DocumentType.FaturaDokum;
+                    break;
+                case "HastaBilgiFormu":
+                    d_type = DocumentType.HastaBilgiFormu;
+                    break;
+                case "ibraname":
+                    d_type = DocumentType.ibraname;
+                    break;
+                case "KonsultasyonRp":
+                    d_type = DocumentType.KonsultasyonRp;
+                    break;
+                case "PatolojiSonuc":
+                    d_type = DocumentType.PatolojiSonuc;
+                    break;
+                case "PolklinikKaydi":
+                    d_type = DocumentType.PolklinikKaydi;
+                    break;
+                case "Recete":
+                    d_type = DocumentType.Recete;
+                    break;
+                case "SigortaliBeyan":
+                    d_type = DocumentType.SigortaliBeyan;
+                    break;
+                case "TedaviPlani":
+                    d_type = DocumentType.TedaviPlani;
+                    break;
+                case "TetkikSonucu":
+                    d_type = DocumentType.TetkikSonucu;
+                    break;
+                default:
+                    d_type = DocumentType.DigerTazmEvr;
+                    break;
+            }
+            dosyaTipi.Text = GetDocumentType(d_type);
+
+            string uztip = textBox2.Text;
+            MimeType m_type;
+            switch (uztip)
+            {
+                case "doc":
+                    m_type = MimeType.doc;
+                    break;
+                case "excel":
+                    m_type = MimeType.excel;
+                    break;
+                case "msword":
+                    m_type = MimeType.msword;
+                    break;
+                case "pdf":
+                    m_type = MimeType.pdf;
+                    break;
+                case "png":
+                    m_type = MimeType.png;
+                    break;
+                case "sheet":
+                    m_type = MimeType.sheet;
+                    break;
+                default:
+                    m_type = MimeType.jpeg;
+                    break;
+            }
+            dosyaUzantisi.Text = GetDocumentType(m_type);
+        }
+    }
+}
